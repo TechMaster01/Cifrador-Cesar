25886e5b8dc750d63cca6cb7717eaf4ffa2427fd
Mario Evenezer <me.luna20@info.uas.edu.mx>
2025-04-01 00:34:21 -0700

    Correcciones

--- index.js
+++ index.js
@@ -1,58 +1,52 @@
 let key = 7;
-let abecedario = [...'aábcdeéfghiíjklmnñoópqrstuúüvwxyzAÁBCDEÉFGHIÍJKLMNÑOÓPQRSTUÚÜVWXYZ0123456789 aábcdeéfghiíjklmnñoópqrstuúüvwxyzAÁBCDEÉFGHIÍJKLMNÑOÓPQRSTUÚÜVWXYZ0123456789 '];
+let abecedario = [...'aábcdeéfghiíjklmnñoópqrstuúüvwxyzAÁBCDEÉFGHIÍJKLMNÑOÓPQRSTUÚÜVWXYZ0123456789 '];
 
-
-function cifrar(){
+function cifrar() {
     let Message = document.getElementById("Mensaje").value;
-    let Longitud = Message.length;
-    let MessageCifrado = [];
-    
-    if(Longitud == 0){
+    let MessageCifrado = "";
+
+    if (Message.length === 0) {
         alert("Escriba un mensaje por favor!");
         return;
     }
 
-    document.getElementById("EncabezadoMessage").innerHTML = "El mensaje cifrado es:"
+    document.getElementById("EncabezadoMessage").innerHTML = "El mensaje cifrado es:";
 
-    
-    for(i=0; i<Longitud; i++){
-        for(j=0; j<65; j++){
-            if(Message[i] == abecedario[j]){
-                MessageCifrado[i] = abecedario[j+key];
-            }
+    for (let i = 0; i < Message.length; i++) {
+        let index = abecedario.indexOf(Message[i]);
+        if (index !== -1) {
+            let newIndex = (index + key) % abecedario.length;
+            MessageCifrado += abecedario[newIndex];
+        } else {
+            MessageCifrado += Message[i];
         }
     }
 
-    MessageCifrado = MessageCifrado.join("");
-
-    /* MessageCifrado = MessageCifrado.replace(/,/g, ''); */
     console.log(MessageCifrado);
-
     document.getElementById("MessageEncrypt").innerHTML = MessageCifrado;
 }
 
-function descifrar(){
+function descifrar() {
     let Message = document.getElementById("Mensaje").value;
-    let Longitud = Message.length;
-    let MessageDescifrado = [];
-    
-    if(Longitud == 0){
+    let MessageDescifrado = "";
+
+    if (Message.length === 0) {
         alert("Escriba un mensaje por favor!");
         return;
     }
 
-    for(i=0; i<Longitud; i++){
-        for(j=65; j<abecedario.length; j++){
-            if(Message[i] == abecedario[j]){
-                MessageDescifrado[i] = abecedario[j-key];
-            }
+    document.getElementById("EncabezadoMessage").innerHTML = "El mensaje descifrado es:";
+
+    for (let i = 0; i < Message.length; i++) {
+        let index = abecedario.indexOf(Message[i]);
+        if (index !== -1) {
+            let newIndex = (index - key + abecedario.length) % abecedario.length;
+            MessageDescifrado += abecedario[newIndex];
+        } else {
+            MessageDescifrado += Message[i];
         }
     }
 
-    MessageDescifrado = MessageDescifrado.join("");
     console.log(MessageDescifrado);
-    
-    document.getElementById("EncabezadoMessage").innerHTML = "El mensaje descifrado es:"
-
     document.getElementById("MessageEncrypt").innerHTML = MessageDescifrado;
 }
